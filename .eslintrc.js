029ba35a422e9e90fd635bc4c8e3fecea990c695
Vladimir <vladimir@Air-Vladimir.Dlink>
2025-06-21 13:50:09 +0300

    [N/A] init app

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,58 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'react-native',
+    'react-hooks',
+    'unused-imports',
+  ],
+  rules: {
+    'no-console': ['error', { allow: ['error'] }],
+    'no-nested-ternary': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react-hooks/rules-of-hooks': 'warn',
+    'no-unused-vars': 2,
+    'unused-imports/no-unused-imports': 'error',
+    'react/prop-types': 'off',
+    'unused-imports/no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        argsIgnorePattern: '^_',
+      },
+    ],
+    'no-var': 1,
+  },
+  overrides: [
+    {
+      files: ['**/*.ts?(x)'],
+      rules: {
+        '@typescript-eslint/no-unused-vars': [
+          'warn',
+          {
+            vars: 'all',
+            varsIgnorePattern: '^_',
+            argsIgnorePattern: '^_',
+          },
+        ],
+      },
+    },
+  ],
 };
